3acc58608f96db3edcd37a8cef2c175f21b250da
shmuelmb <shmuel.cohen0@gmail.com>
2023-02-05 21:17:16 +0200

    remove lines in index.js

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ const app = express();
 // middlewares for the server
 app.use(express.json());
 app.use(cors());
-app.use(express.static("client/build"));
+
 
 mongoose.set("strictQuery", false);
 
-app.get("*"),
-  (req, res) => {
-    res.sendFile(_dirname + "/client/build/index.html");
-  };
+
 
 app.listen(PORT, () => {
   console.log("i am listening on port " + PORT);
