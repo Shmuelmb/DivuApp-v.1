aa19d497ff1552a7fa363b08cb260a11e7a4dab7
NatiRey619 <NatiRey619@gmail.com>
2023-02-07 11:52:40 +0200

    added time per button shift

--- client/src/components/HomePageButtons/StartShift.js
+++ client/src/components/HomePageButtons/StartShift.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 //HOOK Always on top of component
 const StartShift = () => {
@@ -6,6 +6,35 @@ const StartShift = () => {
 
     
     // use state is condition for true fase
+
+    function giveStartShift () {
+
+        const currentStart = {startShift}
+        console.log(currentStart)
+
+
+    }
+
+    useEffect(() => {
+        const date = new Date().getDate() //current date
+        const month = new Date().getMonth() + 1 //current month
+        const year = new Date().getFullYear() //current year
+        const hours = new Date().getHours() //current hours
+        const min = new Date().getMinutes() //current minutes
+        const sec = new Date().getSeconds() //current seconds
+        setStartShift(
+            date + '/' + month + '/' + year + '    ' +  hours + ':' + min + ':' + sec
+        )
+    }, )
+
+    useEffect(() => {
+        let id = setInterval(() => {
+            setStartShift(new Date().toLocaleTimeString());
+        }, 1000);
+    
+        return () => clearInterval(id);
+      }, []);
+
     const isFan = false
 
     const toggleIsAdmin = () => {
@@ -15,7 +44,8 @@ const StartShift = () => {
     return (
         <div>
        
-            <button>התחל משמרת</button>
+            <button onClick={giveStartShift}>התחל משמרת</button>
+            <p>{startShift}</p>
             
 
 
