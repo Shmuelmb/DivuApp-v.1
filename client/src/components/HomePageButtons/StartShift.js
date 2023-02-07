97d62a6276b5446862021ebb4a2e21281649199e
NatiRey619 <NatiRey619@gmail.com>
2023-02-07 15:53:05 +0200

    clock for buttons need to adjust only when click

--- client/src/components/HomePageButtons/StartShift.js
+++ client/src/components/HomePageButtons/StartShift.js
@@ -9,23 +9,14 @@ const StartShift = () => {
 
     function giveStartShift () {
 
-        const currentStart = {startShift}
+        const currentStart = [startShift]
         console.log(currentStart)
+
 
 
     }
 
-    useEffect(() => {
-        const date = new Date().getDate() //current date
-        const month = new Date().getMonth() + 1 //current month
-        const year = new Date().getFullYear() //current year
-        const hours = new Date().getHours() //current hours
-        const min = new Date().getMinutes() //current minutes
-        const sec = new Date().getSeconds() //current seconds
-        setStartShift(
-            date + '/' + month + '/' + year + '    ' +  hours + ':' + min + ':' + sec
-        )
-    }, )
+
 
     useEffect(() => {
         let id = setInterval(() => {
@@ -35,17 +26,12 @@ const StartShift = () => {
         return () => clearInterval(id);
       }, []);
 
-    const isFan = false
-
-    const toggleIsAdmin = () => {
-    }
-    
     //js logic
     return (
         <div>
-       
+                 
             <button onClick={giveStartShift}>התחל משמרת</button>
-            <p>{startShift}</p>
+            <p>time of shift clicked : {startShift} </p>
             
 
 
