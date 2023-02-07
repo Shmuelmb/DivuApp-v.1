ee7d2b8377a1cdb05128b6cfb360baddd551efa2
shmuel c <shmuel.cohen0@gmail.com>
2023-02-07 18:51:02 +0200

    fix what almog does

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLoginPage from "./components/UserLoginPage/UserLoginPage";
 import Footer from "./components/UserLoginPage/Footer/Footer";
-import HomePage from "./components/HomePage/HomePage.js"
+import HomePage from "./components/HomePage/HomePage.js";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserLoginPage />} />
-<Route path="homepage" element={<HomePage />}/>
+        <Route path="homepage" element={<HomePage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- client/src/components/HomePageNav/HomePageNav.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HomePageNav = () => {
-  return <div>HomePageNav</div>;
-};
-
-export default HomePageNav;
