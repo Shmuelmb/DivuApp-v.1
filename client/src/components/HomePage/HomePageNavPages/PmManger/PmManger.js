f7460c51d2243a54bc4892770e90e70d437ef799
NatiRey619 <NatiRey619@gmail.com>
2023-02-08 15:20:08 +0200

    changing pm manager

--- client/src/components/HomePage/HomePageNavPages/PmManger/PmManger.js
+++ client/src/components/HomePage/HomePageNavPages/PmManger/PmManger.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const PmManger = () => {
-  return <div className="pm-manager">PmManger</div>;
+  return <div className="pm-manager">
+    
+    <h1>PM Manager</h1>
+    <p>send a message</p>
+    
+    </div>;
 };
 
 export default PmManger;
