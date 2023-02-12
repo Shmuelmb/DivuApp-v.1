6fc07015cc72196ad686d704360465e9b0f01380
natirey619 <natirey619@gmail.com>
2023-02-12 18:13:27 +0200

    check

--- client/src/components/HomePage/HomeBody/EndShift/EndShift.js
+++ client/src/components/HomePage/HomeBody/EndShift/EndShift.js
@@ -5,16 +5,27 @@ const EndShift = () => {
   const [endShift, setEndShift] = useState("");
   const [disable, setDisable] = useState(false);
 
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
+
   function giveEndShift() {
     setEndShift(new Date().toLocaleTimeString());
-    console.log(endShift)
-    setDisable(true)
+    const object = {
+      ID: "ID HERE",
+      StartShift: { endShift, date },
+    };
+    console.log(object);
+    setDisable(true);
   }
 
   //js logic
   return (
     <div className="end-shift">
-      <button disabled={disable} onClick={giveEndShift}>סיים משמרת</button>
+      <button disabled={disable} onClick={giveEndShift}>
+        סיים משמרת
+      </button>
       <p>End time :{endShift}</p>
     </div>
   );
